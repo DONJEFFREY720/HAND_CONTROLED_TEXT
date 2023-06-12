7c7612a40a094b2f5cb7353ab4654e50f1ad065a
DON JEFFREY <95631932+DONJEFFREY720@users.noreply.github.com>
2023-06-12 18:33:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+function setup(){
+     canvas = createCanvas(400,400)
+     canvas.position(560,150)
+     video = createCapture(VIDEO)
+     video.size(550,500)
+     poseNet = ml5.poseNet(video,modelLoaded)
+     poseNet.on("pose",gotResults)
+}
+
+function gotResults(results){
+     if(results.length>0){
+          console.log(results)
+     }
+}
+
+function modelLoaded(){
+     console.log("MODEL READY!")
+}
+
+function draw(){
+     background("grey")
+}
